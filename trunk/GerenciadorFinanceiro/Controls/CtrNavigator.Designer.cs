3d3c0b158eb12006b3599aa6d4fbc4534e0acde2
alcenir.sis <alcenir.sis@bd3c5764-68c0-11de-bf08-17995f091a54>
2009-07-07 01:15:09 +0000

    Alterações feitas no formulario TipoVeiculo com o modelo de controle de botões da interface. Quase pronto aguardando comprovação do nosso amigo Danimar.....

--- trunk/GerenciadorFinanceiro/Controls/CtrNavigator.Designer.cs
+++ trunk/GerenciadorFinanceiro/Controls/CtrNavigator.Designer.cs
@@ -29,37 +29,105 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.BtnProximo = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.BtnExcluir = new System.Windows.Forms.Button();
+            this.BtnSalvar = new System.Windows.Forms.Button();
+            this.BtnCancelar = new System.Windows.Forms.Button();
+            this.BtnEditar = new System.Windows.Forms.Button();
+            this.BtnNovo = new System.Windows.Forms.Button();
             this.BtnUltimo = new System.Windows.Forms.Button();
+            this.BtnProximo = new System.Windows.Forms.Button();
             this.BtnAnterior = new System.Windows.Forms.Button();
             this.BtnPrimeiro = new System.Windows.Forms.Button();
-            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.PnlBotoes = new System.Windows.Forms.Panel();
+            this.PnlBotoes.SuspendLayout();
             this.SuspendLayout();
             // 
-            // BtnProximo
+            // toolTip
             // 
-            this.BtnProximo.Image = global::GerenciadorFinanceiro.Properties.Resources.proximo;
-            this.BtnProximo.Location = new System.Drawing.Point(85, 2);
-            this.BtnProximo.Name = "BtnProximo";
-            this.BtnProximo.Size = new System.Drawing.Size(35, 28);
-            this.BtnProximo.TabIndex = 2;
-            this.toolTip.SetToolTip(this.BtnProximo, "Próximo");
-            this.BtnProximo.UseVisualStyleBackColor = true;
+            this.toolTip.IsBalloon = true;
+            this.toolTip.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // BtnExcluir
+            // 
+            this.BtnExcluir.Image = global::GerenciadorFinanceiro.Properties.Resources.trash;
+            this.BtnExcluir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnExcluir.Location = new System.Drawing.Point(463, 3);
+            this.BtnExcluir.Name = "BtnExcluir";
+            this.BtnExcluir.Size = new System.Drawing.Size(70, 28);
+            this.BtnExcluir.TabIndex = 8;
+            this.BtnExcluir.Text = "     Excluir";
+            this.BtnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // BtnSalvar
+            // 
+            this.BtnSalvar.Image = global::GerenciadorFinanceiro.Properties.Resources.disc;
+            this.BtnSalvar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnSalvar.Location = new System.Drawing.Point(388, 3);
+            this.BtnSalvar.Name = "BtnSalvar";
+            this.BtnSalvar.Size = new System.Drawing.Size(70, 28);
+            this.BtnSalvar.TabIndex = 7;
+            this.BtnSalvar.Text = "     Salvar";
+            this.BtnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancelar
+            // 
+            this.BtnCancelar.Image = global::GerenciadorFinanceiro.Properties.Resources.Cancel;
+            this.BtnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnCancelar.Location = new System.Drawing.Point(313, 3);
+            this.BtnCancelar.Name = "BtnCancelar";
+            this.BtnCancelar.Size = new System.Drawing.Size(70, 28);
+            this.BtnCancelar.TabIndex = 6;
+            this.BtnCancelar.Text = "Cancelar";
+            this.BtnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // BtnEditar
+            // 
+            this.BtnEditar.Image = global::GerenciadorFinanceiro.Properties.Resources.file_edit;
+            this.BtnEditar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnEditar.Location = new System.Drawing.Point(238, 3);
+            this.BtnEditar.Name = "BtnEditar";
+            this.BtnEditar.Size = new System.Drawing.Size(70, 28);
+            this.BtnEditar.TabIndex = 5;
+            this.BtnEditar.Text = "    Editar";
+            this.BtnEditar.UseVisualStyleBackColor = true;
+            // 
+            // BtnNovo
+            // 
+            this.BtnNovo.Image = global::GerenciadorFinanceiro.Properties.Resources.file_add;
+            this.BtnNovo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnNovo.Location = new System.Drawing.Point(163, 3);
+            this.BtnNovo.Name = "BtnNovo";
+            this.BtnNovo.Size = new System.Drawing.Size(70, 28);
+            this.BtnNovo.TabIndex = 4;
+            this.BtnNovo.Text = "     Novo";
+            this.BtnNovo.UseVisualStyleBackColor = true;
             // 
             // BtnUltimo
             // 
             this.BtnUltimo.Image = global::GerenciadorFinanceiro.Properties.Resources.ultimo;
-            this.BtnUltimo.Location = new System.Drawing.Point(125, 2);
+            this.BtnUltimo.Location = new System.Drawing.Point(123, 3);
             this.BtnUltimo.Name = "BtnUltimo";
             this.BtnUltimo.Size = new System.Drawing.Size(35, 28);
             this.BtnUltimo.TabIndex = 3;
             this.toolTip.SetToolTip(this.BtnUltimo, "Último");
             this.BtnUltimo.UseVisualStyleBackColor = true;
             // 
+            // BtnProximo
+            // 
+            this.BtnProximo.Image = global::GerenciadorFinanceiro.Properties.Resources.proximo;
+            this.BtnProximo.Location = new System.Drawing.Point(83, 3);
+            this.BtnProximo.Name = "BtnProximo";
+            this.BtnProximo.Size = new System.Drawing.Size(35, 28);
+            this.BtnProximo.TabIndex = 2;
+            this.toolTip.SetToolTip(this.BtnProximo, "Próximo");
+            this.BtnProximo.UseVisualStyleBackColor = true;
+            // 
             // BtnAnterior
             // 
             this.BtnAnterior.Image = global::GerenciadorFinanceiro.Properties.Resources.anterior;
-            this.BtnAnterior.Location = new System.Drawing.Point(45, 2);
+            this.BtnAnterior.Location = new System.Drawing.Point(43, 3);
             this.BtnAnterior.Name = "BtnAnterior";
             this.BtnAnterior.Size = new System.Drawing.Size(35, 28);
             this.BtnAnterior.TabIndex = 1;
@@ -69,28 +137,37 @@
             // BtnPrimeiro
             // 
             this.BtnPrimeiro.Image = global::GerenciadorFinanceiro.Properties.Resources.primeiro;
-            this.BtnPrimeiro.Location = new System.Drawing.Point(5, 2);
+            this.BtnPrimeiro.Location = new System.Drawing.Point(3, 3);
             this.BtnPrimeiro.Name = "BtnPrimeiro";
             this.BtnPrimeiro.Size = new System.Drawing.Size(35, 28);
             this.BtnPrimeiro.TabIndex = 0;
             this.toolTip.SetToolTip(this.BtnPrimeiro, "Primeiro");
             this.BtnPrimeiro.UseVisualStyleBackColor = true;
             // 
-            // toolTip
+            // PnlBotoes
             // 
-            this.toolTip.IsBalloon = true;
-            this.toolTip.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.PnlBotoes.Controls.Add(this.BtnPrimeiro);
+            this.PnlBotoes.Controls.Add(this.BtnExcluir);
+            this.PnlBotoes.Controls.Add(this.BtnAnterior);
+            this.PnlBotoes.Controls.Add(this.BtnSalvar);
+            this.PnlBotoes.Controls.Add(this.BtnProximo);
+            this.PnlBotoes.Controls.Add(this.BtnCancelar);
+            this.PnlBotoes.Controls.Add(this.BtnUltimo);
+            this.PnlBotoes.Controls.Add(this.BtnEditar);
+            this.PnlBotoes.Controls.Add(this.BtnNovo);
+            this.PnlBotoes.Location = new System.Drawing.Point(4, 4);
+            this.PnlBotoes.Name = "PnlBotoes";
+            this.PnlBotoes.Size = new System.Drawing.Size(538, 34);
+            this.PnlBotoes.TabIndex = 9;
             // 
             // CtrNavigator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.BtnUltimo);
-            this.Controls.Add(this.BtnProximo);
-            this.Controls.Add(this.BtnAnterior);
-            this.Controls.Add(this.BtnPrimeiro);
+            this.Controls.Add(this.PnlBotoes);
             this.Name = "CtrNavigator";
-            this.Size = new System.Drawing.Size(164, 33);
+            this.Size = new System.Drawing.Size(547, 42);
+            this.PnlBotoes.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -102,5 +179,11 @@
         private System.Windows.Forms.Button BtnProximo;
         private System.Windows.Forms.Button BtnUltimo;
         private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Button BtnNovo;
+        private System.Windows.Forms.Button BtnEditar;
+        private System.Windows.Forms.Button BtnCancelar;
+        private System.Windows.Forms.Button BtnSalvar;
+        private System.Windows.Forms.Button BtnExcluir;
+        private System.Windows.Forms.Panel PnlBotoes;
     }
 }
